38ab98dc5a9ea9768a345969aab8db6688496dea
jose-delgado-git <nimascript@proton.me>
2025-09-26 14:44:50 +0000

    Modification de config/middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,24 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'frame-ancestors': ["'self'", 'https://delgado-psychopraticien.fr'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://delgado-psychopraticien.fr'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
